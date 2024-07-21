a6d449500e539b71f0ecf6f446b126878d23576d
Erik Fong <quantumerikfong@gmail.com>
2024-07-21 17:10:22 -0400

    Made the needed changes since everything is an owned value now.

--- src/pools.rs
+++ src/pools.rs
@@ -3,7 +3,7 @@ use std::collections::hash_map::RandomState;
 
 use indexmap::IndexMap;
 
-use crate::bout::{Bout, FencerVs, FencerVsError};
+use crate::bout::{Bout, FencerScore, FencerVs, FencerVsError};
 use crate::fencer::Fencer;
 use crate::organizations::usafencing::pool_bout_orders::{get_default_order, PoolOrderError};
 
@@ -27,12 +27,12 @@ impl<T: Fencer> BoutsCreator<T> for SimpleBoutsCreator {
 }
 
 #[derive(Debug)]
-pub struct PoolSheet<'a, T: Fencer> {
+pub struct PoolSheet<T: Fencer> {
     fencers: Vec<T>,
-    pub bouts: RefCell<IndexMap<FencerVs<'a, T>, Bout<'a, T>, RandomState>>,
+    pub bouts: IndexMap<FencerVs<T>, Bout<T>, RandomState>,
 }
 
-impl<'a, 'b, T: Fencer> PoolSheet<'a, T> {
+impl<T: Fencer> PoolSheet<T> {
     pub fn add_fencer(&mut self, fencer: T) {
         self.fencers.push(fencer);
     }
@@ -44,53 +44,71 @@ impl<'a, 'b, T: Fencer> PoolSheet<'a, T> {
         self.fencers.extend(fencers);
     }
 
-    pub fn get_fencers(&self) -> &Vec<T>{
+    pub fn get_fencers(&self) -> &Vec<T> {
         &self.fencers
     }
 
-    pub fn create_bouts<C>(&'b self, creator: &C) -> Result<(), BoutCreationError>
+    pub fn iter(&self) -> indexmap::map::Iter<FencerVs<T>, Bout<T>> {
+        self.bouts.iter()
+    }
+
+    pub fn create_bouts<C>(&mut self, creator: &C) -> Result<(), BoutCreationError>
     where
         C: BoutsCreator<T>,
-        'b: 'a
     {
         match creator.get_order(&self.fencers) {
             Ok(bout_indexes) => {
-                let mut bouts_map = self.bouts.borrow_mut();
                 for pair in bout_indexes.into_iter() {
                     match FencerVs::new(
-                        self.fencers.get(pair.0-1).unwrap(),
-                        self.fencers.get(pair.1-1).unwrap()
+                        self.fencers.get(pair.0 - 1).unwrap().clone(),
+                        self.fencers.get(pair.1 - 1).unwrap().clone(),
                     ) {
                         Ok(versus) => {
-                            bouts_map.insert(versus.clone(), Bout::new(versus));
-                        },
+                            self.bouts.insert(versus.clone(), Bout::new(versus));
+                        }
                         Err(err) => {
-                            return Err(BoutCreationError::VsError(err, "The pool creation paired a fencer with themselves.".to_string()))
+                            return Err(BoutCreationError::VsError(
+                                err,
+                                "The pool creation paired a fencer with themselves.".to_string(),
+                            ))
                         }
                     }
                 }
                 Ok(())
             }
-            Err(err) => {
-                Err(BoutCreationError::PoolOrderError(err))
-            }
+            Err(err) => Err(BoutCreationError::PoolOrderError(err)),
         }
+    }
+
+    pub fn update_score(
+        &mut self,
+        fencer_a: FencerScore<T>,
+        fencer_b: FencerScore<T>,
+    ) -> Result<(), PoolOrderError> {
+        let x = FencerVs::new(fencer_a.fencer.clone(), fencer_b.fencer.clone()).unwrap();
+        let bout = self.bouts.get_mut(&x).unwrap();
+        bout.update_score(fencer_a, fencer_b);
+        Ok(())
     }
 }
 
-impl<'a, T: Fencer> Default for PoolSheet<'a, T> {
+impl<T: Fencer> Default for PoolSheet<T> {
     fn default() -> Self {
         PoolSheet {
             fencers: Vec::new(),
-            bouts: RefCell::new(IndexMap::new()),
+            bouts: IndexMap::new(),
         }
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::{bout::{FencerScore, FencerVs}, cards::Cards, fencer::SimpleFencer};
     use super::{PoolSheet, SimpleBoutsCreator};
+    use crate::{
+        bout::{FencerScore, FencerVs},
+        cards::Cards,
+        fencer::SimpleFencer,
+    };
 
     #[test]
     fn iter_test() {
@@ -104,7 +122,7 @@ mod tests {
         let mut pool_sheet = PoolSheet::default();
         pool_sheet.add_fencers(fencers.into_iter());
         let _ = pool_sheet.create_bouts(&SimpleBoutsCreator);
-        for bout in pool_sheet.bouts.borrow().iter() {
+        for bout in pool_sheet.iter() {
             println!("{bout:#?}");
         }
     }
@@ -118,26 +136,29 @@ mod tests {
             SimpleFencer::new("Fencer4"),
         ];
 
-        let json_fencer1 = serde_json::from_str::<SimpleFencer>(r#"{"name":"Fencer1","clubs":[]}"#).unwrap();
-        let json_fencer2 = serde_json::from_str::<SimpleFencer>(r#"{"name":"Fencer2","clubs":[]}"#).unwrap();
+        let json_fencer1 =
+            serde_json::from_str::<SimpleFencer>(r#"{"name":"Fencer1","clubs":[]}"#).unwrap();
+        let json_fencer2 =
+            serde_json::from_str::<SimpleFencer>(r#"{"name":"Fencer2","clubs":[]}"#).unwrap();
 
         let mut pool_sheet = PoolSheet::default();
         pool_sheet.add_fencers(fencers.clone().into_iter());
         let _ = pool_sheet.create_bouts(&SimpleBoutsCreator);
 
-        let a_versus = FencerVs::new(&json_fencer1, &json_fencer2).unwrap();
+        let a_versus = FencerVs::new(json_fencer1.clone(), json_fencer2.clone()).unwrap();
 
-        let mut bouts = pool_sheet.bouts.borrow_mut();
-        let a_bout = bouts.get_mut(&a_versus).unwrap();
-        a_bout.update_score(FencerScore {
-            fencer: &json_fencer1,
-            score: 0,
-            cards: Cards::default(),
-        },FencerScore {
-            fencer: &json_fencer2,
-            score: 0,
-            cards: Cards::default(),
-        });
-        println!("\nSingle Bout: {a_bout:#?}");
+        let smth = pool_sheet.update_score(
+            FencerScore {
+                fencer: json_fencer1,
+                score: 0,
+                cards: Cards::default(),
+            },
+            FencerScore {
+                fencer: json_fencer2,
+                score: 0,
+                cards: Cards::default(),
+            },
+        );
+        println!("\nSingle Bout: {smth:#?}");
     }
 }
