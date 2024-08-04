f33b933cb90b655688d6efce2072d09c0a24f345
Erik Fong <quantumerikfong@gmail.com>
2024-08-04 19:51:25 -0400

    Documentation for FencerScore

--- src/bout/score.rs
+++ src/bout/score.rs
@@ -1,6 +1,19 @@
 use crate::{cards::Cards, fencer::Fencer};
 use std::{borrow::Borrow, marker::PhantomData};
 
+/// A struct to associate a Fencer with a score and cards.
+/// # Usage
+/// ```
+/// use fencing_sport_lib::{fencer::SimpleFencer, bout::FencerScore, cards::Cards};
+/// let fencer_a = SimpleFencer::new("Alice");
+///
+/// // Like many structs in this library, need to explicitly type it
+/// let versus: FencerScore<SimpleFencer, SimpleFencer> = FencerScore::new(fencer_a, 1, Cards::default());
+///
+/// // Because you can also wrap the fencer type with smart pointers.
+/// let fencer_a = Box::new(SimpleFencer::new("Alice"));
+/// let versus: FencerScore<SimpleFencer, _> = FencerScore::new(fencer_a, 1, Cards::default());
+/// ```
 #[derive(Debug, Clone)]
 pub struct FencerScore<U: Fencer, T: Borrow<U>> {
     pub(crate) fencer: T,
@@ -10,6 +23,7 @@ pub struct FencerScore<U: Fencer, T: Borrow<U>> {
 }
 
 impl<U: Fencer, T: Borrow<U>> FencerScore<U, T> {
+    /// Create a new instance of the FencerScore struct.
     pub fn new(fencer: T, score: u8, cards: Cards) -> FencerScore<U, T> {
         FencerScore {
             fencer,
