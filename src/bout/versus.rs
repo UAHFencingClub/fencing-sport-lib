18c4bd2aa79a7e03dca29c496eec00d996e3bbe5
Erik Fong <quantumerikfong@gmail.com>
2024-08-04 19:44:00 -0400

    Documentation for FencerVs

--- src/bout/versus.rs
+++ src/bout/versus.rs
@@ -12,10 +12,32 @@ pub(crate) enum TuplePos {
     None,
 }
 
+/// This struct holds 2 fencers that will be competing against each other in a bout.
+/// It is defined to enforce that the fencers passed in are not the same.
+///
+/// It is also defined so FencerVs structs with the same fencers but in different orders hash the same and are equal.
+///
+/// # Usage
+/// ```
+/// use fencing_sport_lib::{fencer::SimpleFencer, bout::FencerVs};
+/// let fencer_a = SimpleFencer::new("Alice");
+/// let fencer_b = SimpleFencer::new("Bob");
+///
+/// // Like many structs in this library, need to explicitly type it
+/// let versus: FencerVs<SimpleFencer, SimpleFencer> = FencerVs::new(fencer_a, fencer_b).unwrap();
+///
+/// // Because you can also wrap the type with smart pointers.
+/// let fencer_a = Box::new(SimpleFencer::new("Alice"));
+/// let fencer_b = Box::new(SimpleFencer::new("Bob"));
+/// let versus: FencerVs<SimpleFencer, _> = FencerVs::new(fencer_a, fencer_b).unwrap();
+/// ```
 #[derive(Debug, Eq, Clone)]
 pub struct FencerVs<U: Fencer, T: Borrow<U> + Clone>(pub T, pub T, PhantomData<U>);
 
 impl<U: Fencer, T: Borrow<U> + Clone> FencerVs<U, T> {
+    /// Initializes a new instance of FencerVs
+    /// Errors if `fencer_a  == fencer_b`
+    /// Type will need to be specified due to the way I have done the generics.
     pub fn new(fencer_a: T, fencer_b: T) -> Result<Self, PoolSheetError> {
         if fencer_a.borrow() == fencer_b.borrow() {
             Err(PoolSheetError::InvalidBout)
@@ -24,6 +46,8 @@ impl<U: Fencer, T: Borrow<U> + Clone> FencerVs<U, T> {
         }
     }
 
+    /// Returns a cloned version of the specified fencer if in the struct.
+    /// I do not quite remember why I wanted this.
     pub fn get_fencer<A: Borrow<U>>(&self, fencer: &A) -> Option<T> {
         match self.pos(fencer.borrow()) {
             TuplePos::First => Some(self.0.clone()),
@@ -32,6 +56,7 @@ impl<U: Fencer, T: Borrow<U> + Clone> FencerVs<U, T> {
         }
     }
 
+    /// Returns the which position in the tuple struct the fencer is in.
     pub(crate) fn pos<A: Borrow<U>>(&self, fencer: &A) -> TuplePos {
         if fencer.borrow() == self.0.borrow() {
             TuplePos::First
@@ -42,6 +67,7 @@ impl<U: Fencer, T: Borrow<U> + Clone> FencerVs<U, T> {
         }
     }
 
+    /// Used to create a `FencerVs<U, &U>` which can be useful for comparisons.
     pub fn ref_type(&self) -> FencerVs<U, &U> {
         FencerVs(self.0.borrow(), self.1.borrow(), PhantomData)
     }
